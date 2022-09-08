02760a9a5af755548ea84cb973b8aa147ad6bac5
Cintrian <velegoj2@yandex.ru>
2022-09-08 21:44:47 +0300

    Task5

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-       int deliveryDistance = 95;
-       int deliveryDays = 0;
-       if (deliveryDistance <= 20) {
-           deliveryDays = 1;
-       } else if (deliveryDistance<=60) {
-           deliveryDays = 2;
-       } else if (deliveryDistance<=100) {
-           deliveryDays = 3;
+       int monthNumber = 12;
+       switch (monthNumber) {
+           case 1:
+           case 2:
+           case 12:
+               System.out.println("Зима");
+               break;
+           case 3:
+           case 4:
+           case 5:
+               System.out.println("Весна");
+               break;
+           case 6:
+           case 7:
+           case 8:
+               System.out.println("Лето");
+               break;
+           case 9:
+           case 10:
+           case 11:
+               System.out.println("Осень");
+               break;
+           default:
+               System.out.println("Такого месяца нет");
        }
-        System.out.println("Потребуется дней " + deliveryDays);
     }
 }
