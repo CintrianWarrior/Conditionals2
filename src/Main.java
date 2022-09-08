67f84ddad99c913821b723891fb1d4ccf05a6e59
Cintrian <velegoj2@yandex.ru>
2022-09-08 20:58:37 +0300

    Task3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 0;
-        int clientDeviceYear = 2015;
-        if (clientOS == 0 && clientDeviceYear >= 2015 && clientDeviceYear <= 2022) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-        if (clientOS == 1&& clientDeviceYear >= 2015 && clientDeviceYear <= 2022) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        if (clientOS==1&&clientDeviceYear < 2015){
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
+       int year = 2000;
+       if (year%4==0 && year%100!=0 || year%400==0) {
+           System.out.println(year + " год является високосным");
+       } else {
+           System.out.println(year + " год не является високосным");
+       }
     }
 }
